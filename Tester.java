fbf89fa0acfe3d737a98961abb51257084b7c7fa
Sarah Leong <sleong20@stuy.edu>
2020-10-23 20:27:28 -0400

    Tested ArrayOps.sumCols() method

--- Tester.java
+++ Tester.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Tester {
   public static void main(String[] args) {
     int[][]  A  =  {  {  1,  0, 12, -1 },
@@ -9,16 +11,19 @@ public class Tester {
 
     // System.out.print(ArrayOps.largest(B));
     
-    int[] testC = ArrayOps.sumRows(A);
-    for (int i = 0; i < testC.length; i++) {
-      System.out.println(testC[i]);
-    }
+    // int[] testC = ArrayOps.sumRows(A);
+    // for (int i = 0; i < testC.length; i++) {
+    //   System.out.println(testC[i]);
+    // }
+    // System.out.print(Arrays.toString(ArrayOps.sumRows(A)));
 
     // int[] testD = ArrayOps.largestInRows(A);
     // for (int i = 0; i < testD.length; i++) {
     //   System.out.println(testD[i]);
     // }
 
-    System.out.print(ArrayOps.sum(A)); 
+    // System.out.print(ArrayOps.sum(A)); 
+
+    System.out.print(Arrays.toString(ArrayOps.sumCols(A)));
   }
 }
